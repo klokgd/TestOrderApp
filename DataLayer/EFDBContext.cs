5f701474ce73555d792519eaa60859f209ab92dc
ll <jokzik@gmail.com>
2022-01-18 14:40:45 +0300

    Add project files.

---
+++ DataLayer/EFDBContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class EFDBContext : DbContext
+    {
+        public DbSet<Order> Order { get; set; }
+
+        public EFDBContext(DbContextOptions<EFDBContext> options) : base(options) { }
+    }
+
+    public class EFDBContextFactory : IDesignTimeDbContextFactory<EFDBContext>
+    {
+        public EFDBContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<EFDBContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=loftBlogASPCoreDb;Trusted_Connection=True;MultipleActiveResultSets=true", b => b.MigrationsAssembly("DataLayer"));
+
+            return new EFDBContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ DataLayer/Entityes/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace DataLayer
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string CityOfSender { get; set; }
+        public string AdressOfSender { get; set; }
+        public string CityOfRecepient { get; set; }
+        public string AdressOfRecepient { get; set; }
+        public double GoodsWeight { get; set; }
+        public DateTime DeliveryDate { get; set; }
+    }
+}
